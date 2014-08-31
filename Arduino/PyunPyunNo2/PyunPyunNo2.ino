a6c4b241cb7796e10f222aad46d04991be4aa414
ryood <o.d.r.ryo@gmail.com>
2014-08-31 17:11:00 +0900

    WaveTable.h ヘッダファイルを追加

--- Arduino/PyunPyunNo2/PyunPyunNo2.ino
+++ Arduino/PyunPyunNo2/PyunPyunNo2.ino
@@ -1,16 +1,16 @@
 /******************************************************
-  Arduino Pyun Pyun Machine Rev.2
- 
+ * Arduino Pyun Pyun Machine Rev.2
+ * 
  * CS   10
  * MOSI 11
  * SCK  13
- 
+ * 
  * Pot Freq      A0
  * Pot LFO Freq  A1
  * Pot LFO Depth A2
  * Button Wave Form A3 (D17)
  * Button LFO Wave  A4 (D18)
- 
+ * 
  * LCD RS pin to digital pin 7
  * LCD Enable pin to digital pin 6
  * LCD D4 pin to digital pin 2
@@ -21,15 +21,15 @@
  * 10K resistor:
  *   ends to +5V and ground
  *   wiper to LCD VO pin (pin 3)
- 
- 2014.01.15 by gizmo
- 2014.01.15 DAC出力
- 2014.01.27 WavForm選択をボタンに
- 2014.01.27 Debounce
- 2014.02.01 LFO表示を3桁に
- 2014.08.25 Rev.2
- 
-********************************************************/
+ * 
+ * 2014.01.15 by gizmo
+ * 2014.01.15 DAC出力
+ * 2014.01.27 WavForm選択をボタンに
+ * 2014.01.27 Debounce
+ * 2014.02.01 LFO表示を3桁に
+ * 2014.08.25 Rev.2
+ * 
+ ********************************************************/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -43,9 +43,9 @@
 #include "WaveTable.h"
 
 #define SERIAL_SPEED 57600
-#define DEBOUNCE_INTERVAL 10
+#define DEBOUNCE_INTERVAL 100
 
-#define DAC_CS_PIN			10
+#define DAC_CS_PIN		10
 #define WAVE_FORM_PIN		17
 #define LFO_FORM_PIN		18
 
@@ -63,7 +63,8 @@
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 
-const char *waveFormStr[] = { "SIN", "TRI", "SW1", "SW2", "SQR" };
+const char *waveFormStr[] = { 
+  "SIN", "TRI", "SW1", "SW2", "SQR" };
 uint16_t *waveForms[WAVEFORM_NUM];
 
 // parameters
@@ -73,9 +74,9 @@ uint8_t lfoAmount    = 0;
 
 /*
 double prevWeveFrequency;
-double prevLfoFrequency;
-uint8_t prevLfoAmount;
-*/
+ double prevLfoFrequency;
+ uint8_t prevLfoAmount;
+ */
 
 int waveForm = 0;
 int lfoForm  = 0;
@@ -94,90 +95,91 @@ volatile uint16_t tick = 0;
 
 void setup()
 {
-	// PINアサインの初期化
-	pinMode(WAVE_FORM_PIN, INPUT_PULLUP);
-	pinMode(LFO_FORM_PIN, INPUT_PULLUP);
-	
-	// Classの初期化
-	Serial.begin(SERIAL_SPEED);
+  // PINアサインの初期化
+  pinMode(WAVE_FORM_PIN, INPUT_PULLUP);
+  pinMode(LFO_FORM_PIN, INPUT_PULLUP);
 
-	lcd.begin(16, 2);
-	
-	MCPDAC.begin(DAC_CS_PIN);
-	MCPDAC.setGain(CHANNEL_A,GAIN_LOW);
-	MCPDAC.shutdown(CHANNEL_A,false);
-	MCPDAC.shutdown(CHANNEL_B,true);
-	
-	bouncerWaveForm.attach(WAVE_FORM_PIN);
-	bouncerWaveForm.interval(DEBOUNCE_INTERVAL);
-	bouncerLfoForm.attach(LFO_FORM_PIN);
-	bouncerLfoForm.interval(DEBOUNCE_INTERVAL);
-	
-	// Timerの初期化
-	SetupTimer2();
-	sbi(TIMSK2, TOIE2);
-	
-	// 変数の初期化
-	lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
-	tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
-	/*
+  // Classの初期化
+  Serial.begin(SERIAL_SPEED);
+
+  lcd.begin(16, 2);
+
+  MCPDAC.begin(DAC_CS_PIN);
+  MCPDAC.setGain(CHANNEL_A,GAIN_LOW);
+  MCPDAC.shutdown(CHANNEL_A,false);
+  MCPDAC.shutdown(CHANNEL_B,true);
+
+  bouncerWaveForm.attach(WAVE_FORM_PIN);
+  bouncerWaveForm.interval(DEBOUNCE_INTERVAL);
+  bouncerLfoForm.attach(LFO_FORM_PIN);
+  bouncerLfoForm.interval(DEBOUNCE_INTERVAL);
+
+  // Timerの初期化
+  SetupTimer2();
+  sbi(TIMSK2, TOIE2);
+
+  // 変数の初期化
+  lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
+  tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
+  /*
 	prevWaveFrequency = waveFrequency;
-	prevLfoFrequency = lfoFrequency;
-	prevLfoAmount = lfoAmount;
-	*/
-	phaseRegister = 0;
-	lfoPhaseRegister = 0;
-	
-	// WaveTableの設定
-	waveForms[0] = waveTableSine;
-	waveForms[1] = waveTableTriangle;
-	waveForms[2] = waveTableSawtoothUp;
-	waveForms[3] = waveTableSawtoothDown;
-	waveForms[4] = waveTableSqure;
+   	prevLfoFrequency = lfoFrequency;
+   	prevLfoAmount = lfoAmount;
+   	*/
+  phaseRegister = 0;
+  lfoPhaseRegister = 0;
 
-	sei();
+  // WaveTableの設定
+  waveForms[0] = waveTableSine;
+  waveForms[1] = waveTableTriangle;
+  waveForms[2] = waveTableSawtoothUp;
+  waveForms[3] = waveTableSawtoothDown;
+  waveForms[4] = waveTableSqure;
+
+  sei();
 }
 
 void loop()
 {
-	char buff[20];
-	
-	// デバイスの読み取り
-	waveFrequency = (double)analogRead(WAVE_FREQUENCY_PIN);		// 0.0..1023.0Hz
-	lfoFrequency  = (double)analogRead(LFO_FREQUENCY_PIN) / 32;	// 0.0..32.0Hz
-	lfoAmount     = map(analogRead(LFO_AMOUNT_PIN), 0, 1023, 0, 255);
-	
-        bouncerWaveForm.update();
-	waveForm += !bouncerWaveForm.read();
-	if (waveForm > WAVEFORM_NUM)
-		waveForm = 0;
-	
-	bouncerLfoForm.update();
-	lfoForm += !bouncerLfoForm.read();
-	if (lfoForm > WAVEFORM_NUM)
-		lfoForm = 0;
-	
-	// 変数の更新
-	tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
-	lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
-	/*
+  char buff[20];
+
+  // デバイスの読み取り
+  waveFrequency = (double)analogRead(WAVE_FREQUENCY_PIN);		// 0.0..1023.0Hz
+  lfoFrequency  = (double)analogRead(LFO_FREQUENCY_PIN) / 32;	// 0.0..32.0Hz
+  lfoAmount     = map(analogRead(LFO_AMOUNT_PIN), 0, 1023, 0, 255);
+
+  if (bouncerWaveForm.update()) {
+    waveForm += !bouncerWaveForm.read();
+    if (waveForm >= WAVEFORM_NUM)
+      waveForm = 0;
+  }
+  if (bouncerLfoForm.update()) {
+    lfoForm += !bouncerLfoForm.read();
+    if (lfoForm >= WAVEFORM_NUM)
+      lfoForm = 0;
+  }
+
+  // 変数の更新
+  tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
+  lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
+  /*
 	if (waveFrequency != prevWaveFrequency) {
-		prevWaveFreqency = waveFrequency;
-		tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
-	}
-	if (lfoFrequency != prevLfoFrequency) {
-		prevLfoFrequency = lfoFrequency;
-		lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
-	}
-	*/
-	
-	// LCDの表示の初期化
-	sprintf(buff, "FREQ LFO DPT %s",  waveFormStr[waveForm]);
-	lcd.setCursor(0, 0);
-    lcd.print(buff);
-    sprintf(buff, "%4d %3d %3d %s", (int)waveFrequency, (int)(lfoFrequency * 10), lfoAmount, waveFormStr[lfoForm]);
-    lcd.setCursor(0, 1);
-    lcd.print(buff); 
+   		prevWaveFreqency = waveFrequency;
+   		tuningWord = waveFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
+   	}
+   	if (lfoFrequency != prevLfoFrequency) {
+   		prevLfoFrequency = lfoFrequency;
+   		lfoTuningWord = lfoFrequency * pow(2.0, 16) / LFO_CLOCK;
+   	}
+   	*/
+
+  // LCDの表示の初期化
+  sprintf(buff, "FREQ LFO DPT %s",  waveFormStr[waveForm]);
+  lcd.setCursor(0, 0);
+  lcd.print(buff);
+  sprintf(buff, "%4d %3d %3d %s", (int)waveFrequency, (int)(lfoFrequency * 10), lfoAmount, waveFormStr[lfoForm]);
+  lcd.setCursor(0, 1);
+  lcd.print(buff); 
 }
 
 //******************************************************************
@@ -185,61 +187,62 @@ void loop()
 // set prscaler to 8, PWM mode to phase correct PWM,  16000000 / 8 / 128 = 15625 Hz clock
 void SetupTimer2()
 {
-	// Timer2 PWM Mode set to Phase Correct PWM
-	/*
+  // Timer2 PWM Mode set to Phase Correct PWM
+  /*
 	cbi (TCCR2A, COM2A0);  
-	cbi (TCCR2A, COM2A1);
-	*/
-	
-	sbi (TCCR2A, WGM20);  // Mode 7 / Fast PWM
-	sbi (TCCR2A, WGM21);
-	sbi (TCCR2B, WGM22);
+   	cbi (TCCR2A, COM2A1);
+   	*/
 
-	OCR2A = 127;
+  sbi (TCCR2A, WGM20);  // Mode 7 / Fast PWM
+  sbi (TCCR2A, WGM21);
+  sbi (TCCR2B, WGM22);
 
-	// Timer2 Clock Prescaler to : 8
-	cbi (TCCR2B, CS20);
-	sbi (TCCR2B, CS21);
-	cbi (TCCR2B, CS22);
+  OCR2A = 127;
+
+  // Timer2 Clock Prescaler to : 8
+  cbi (TCCR2B, CS20);
+  sbi (TCCR2B, CS21);
+  cbi (TCCR2B, CS22);
 }
 
 //******************************************************************
 // Timer2 Interrupt Service at 15,625Hz = 64uSec
 ISR(TIMER2_OVF_vect) {
-	uint16_t lfoIndex;
-        uint16_t lfoValue = 0;
-	uint16_t index;
-        uint16_t waveValue;
-	
-	tick++;
-	
-	// Caluclate LFO Value
+  uint16_t lfoIndex;
+  int16_t lfoValue;
+  uint16_t index;
+  uint16_t waveValue;
 
-	if (tick > SAMPLE_CLOCK / LFO_CLOCK) {
-		tick = 0;
-	
-		lfoPhaseRegister += lfoTuningWord;
-		
-		// 16bitのlfoPhaseRegisterをテーブルの10bit(1024個)に丸める
-		lfoIndex = lfoPhaseRegister >> 6;
+  tick++;
 
-		// lookupTable(12bit) * lfoAmount(8bit) : 20bit -> 16bit
-		lfoValue = (((int32_t)pgm_read_word(waveForms[lfoForm] + lfoIndex)) - 2048) * lfoAmount >> 4;
+  // Caluclate LFO Value
 
-		// tuningWord(16bit) * lfoValue(15bit + 1bit) : (31bit + 1bit) -> 16bit
-		lfoValue = (((int32_t)tuningWord) * lfoValue) >> 15;
-	}
-	
-	// Caluclate Wave Value
-	phaseRegister += tuningWord + lfoValue;
-	
-	// 16bitのphaseRegisterをテーブルの10bit(1024個)に丸める
-	index = phaseRegister >> 6;
-		
-	waveValue = pgm_read_word(waveForms[waveForm] + index);
-        //int t = pgm_read_word(waveForms[waveForm]);
-	
-        //Serial.println(waveValue);
-	// DACに出力
-	MCPDAC.setVoltage(CHANNEL_A, waveValue);
+  if (tick > SAMPLE_CLOCK / LFO_CLOCK) {
+    tick = 0;
+
+    lfoPhaseRegister += lfoTuningWord;
+
+    // 16bitのlfoPhaseRegisterをテーブルの10bit(1024個)に丸める
+    lfoIndex = lfoPhaseRegister >> 6;
+
+    // lookupTable(12bit) * lfoAmount(8bit) : 20bit -> 16bit
+    lfoValue = (((int32_t)pgm_read_word(waveForms[lfoForm] + lfoIndex)) - 2048) * lfoAmount >> 4;
+
+    // tuningWord(16bit) * lfoValue(15bit + 1bit) : (31bit + 1bit) -> 16bit
+    lfoValue = (((int32_t)tuningWord) * lfoValue) >> 15;
+  }
+
+  // Caluclate Wave Value
+  phaseRegister += tuningWord + lfoValue;
+
+  // 16bitのphaseRegisterをテーブルの10bit(1024個)に丸める
+  index = phaseRegister >> 6;
+
+  waveValue = pgm_read_word(waveForms[waveForm] + index);
+  //int t = pgm_read_word(waveForms[waveForm]);
+
+  //Serial.println(waveValue);
+  // DACに出力
+  MCPDAC.setVoltage(CHANNEL_A, waveValue);
 }
+
